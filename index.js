dcca1c69786600cba0b2d773611b510da6ad130d
abir-01 <abir.rakshit@gmail.com>
2023-07-05 17:59:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
+const cors = require("cors")
+
+connectDb();
+
+const corsOptions = {
+    origin: "http://localhost:5173"
+}
+
+const app = express();
+app.use(cors())
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+// app.use("/api/contacts", require("./routes/contactRoutes"));
+// app.use("/api/users", require("./routes/userRoutes"));
+app.get('/', (req, res) => {
+    res.send("Welcome to backend")
+})
+app.use("/api/user", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
---
+++ models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+var validator = require('validator');
+// const isEmail = require('isEmail')
+
+// var validateEmail = function (email) {
+//     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//     return re.test(email)
+// };
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please add the user name"],
+        },
+
+        email: {
+            type: String,
+            trim: true,
+            lowercase: true,
+            unique: true,
+            unique: [true, "Email address already taken!"],
+            required: 'Email address is required',
+            validate: [validator.isEmail, 'Please fill a valid email address'],
+            // match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+
+
+        password: {
+            type: String,
+            required: [true, "Please add the user password"],
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("User", userSchema);
